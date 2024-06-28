5dfc864851111e33bd610a550994d0187287de2f
may <mayjiayiw@gmail.com>
2024-06-28 16:26:32 +0800

    added stylised home page div to take up full page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,8 @@
 const HomePage = () => {
   return(
     <div id="home">
-      <h1>This is the Home page.</h1>
+      <h1>Hello, I'm May</h1>
+      <h1>I am a Software Developer</h1>
     </div>
   );
 }
