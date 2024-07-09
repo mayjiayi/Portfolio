4f62ca8d03dc9f5fcfbaa42565037ba235eef29f
May Lwin <may.lwin@fdmgroup.com>
2024-07-09 22:48:15 +0800

    completed about page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -2,6 +2,41 @@ const AboutPage = () => {
   return(
     <div id="about">
       <h1>About</h1>
+      <p className="about-description">
+        I am a dedicated junior web developer enthusiastic about 
+        building innovative and responsive web applications. Drawn 
+        to the ever-changing landscape of web development, I love seeing 
+        my creations come to life through code. When I'm not busy dealing
+        with bugs, I spend my time reading, or curled up in my bed binge-watching
+        Netflix reality shows.
+        
+      </p>
+      <div className="skills-carousel">
+        <div className="skills-slider">
+          <div className="skill">HTML</div>
+          <div className="skill">CSS</div>
+          <div className="skill">JavaScript</div>
+          <div className="skill">TypeScript</div>
+          <div className="skill">Java</div>
+          <div className="skill">Python</div>
+          <div className="skill">C#</div>
+          <div className="skill">React</div>
+          <div className="skill">MySQL</div>
+          <div className="skill">Git</div>
+        </div>
+        <div className="skills-slider">
+          <div className="skill">HTML</div>
+          <div className="skill">CSS</div>
+          <div className="skill">JavaScript</div>
+          <div className="skill">TypeScript</div>
+          <div className="skill">Java</div>
+          <div className="skill">Python</div>
+          <div className="skill">C#</div>
+          <div className="skill">React</div>
+          <div className="skill">MySQL</div>
+          <div className="skill">Git</div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -2,6 +2,14 @@ const ProjectsPage = () => {
   return(
     <div id="projects">
       <h1>Projects</h1>
+      <div className="project-container">
+        <div className="project-image">
+
+        </div>
+        <div className="project-description">
+          
+        </div>
+      </div>
     </div>
   );
 }
