2c866b54ae037ea8dd3fe5a257b70c164885be62
May Lwin <may.lwin@fdmgroup.com>
2024-07-15 23:10:21 +0800

    added some formatting for projects page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -3,11 +3,24 @@ const ProjectsPage = () => {
     <div id="projects">
       <h1>Projects</h1>
       <div className="project-container">
-        <div className="project-image">
-
-        </div>
+        <img src={process.env.PUBLIC_URL + '/assets/BT.png'} alt="Budget Tracker Screenshot"/>
         <div className="project-description">
-          
+          <h3>Budget Tracker</h3>
+          <p>Full stack web application built with Java, SpringBoot, MySQL</p>
+        </div>
+      </div>
+      <div className="project-container">
+        <img src={process.env.PUBLIC_URL + '/assets/BT.png'} alt="Budget Tracker Screenshot"/>
+        <div className="project-description">
+          <h3>Budget Tracker</h3>
+          <p>Full stack web application built with Java, SpringBoot, MySQL</p>
+        </div>
+      </div>
+      <div className="project-container">
+        <img src={process.env.PUBLIC_URL + '/assets/BT.png'} alt="Budget Tracker Screenshot"/>
+        <div className="project-description">
+          <h3>Budget Tracker</h3>
+          <p>Full stack web application built with Java, SpringBoot, MySQL</p>
         </div>
       </div>
     </div>
